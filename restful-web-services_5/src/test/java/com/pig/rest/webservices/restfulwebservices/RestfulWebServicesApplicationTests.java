c96139b56cbc471d3e84c0686ea84e717ec87212
JavaNoobPig <40035020+JavaNoobPig@users.noreply.github.com>
2019-06-23 17:46:09 +0800

    Udemy 課程Microservices with Spring 14
    Udemy 課程Microservices with Spring 14

---
+++ restful-web-services_5/src/test/java/com/pig/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pig.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
