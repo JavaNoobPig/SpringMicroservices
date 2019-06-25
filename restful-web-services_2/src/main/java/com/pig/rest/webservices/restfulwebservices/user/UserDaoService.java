2a2838bfb56e2e0e54b157012aa73133e40e950c
JavaNoobPig <40035020+JavaNoobPig@users.noreply.github.com>
2019-06-25 23:26:16 +0800

    Udemy 課程Microservices with Spring 5
    Udemy 課程Microservices with Spring 5

---
+++ restful-web-services_2/src/main/java/com/pig/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,44 @@
+package com.pig.rest.webservices.restfulwebservices.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	private static List<User> users = new ArrayList<>();
+
+	private int usersCount = 3;
+
+	static {
+		users.add(new User(1, "Pig", new Date()));
+		users.add(new User(2, "Hog", new Date()));
+		users.add(new User(3, "Royal Hog", new Date()));
+	}
+
+	// public List<User> findAll()
+	public List<User> findAll() {
+		return users;
+	}
+
+	// public User save(User user)
+	public User save(User user) {
+		if (user.getId() == null) {
+			user.setId(++usersCount);
+		}
+		users.add(user);
+		return user;
+	}
+
+	// public User findOne(int id)
+	public User findOne(int id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+}
